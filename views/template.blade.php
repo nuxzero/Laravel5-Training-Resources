bad55c4fd8159de887187cefd02c7988e0887535
Natthawut Hemathulin <natthawut.nux@gmail.com>
2015-06-27 00:36:05 +0700

    Move bootstrap.

---
+++ views/template.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+
+<body>
+    <div style="width:100px; float: left;">
+        <p>Sibebar menu</p>
+        <ul>
+            <li><a href="#">Item 1</a></li>
+            <li><a href="#">Item 2</a></li>
+            <li><a href="#">Item 3</a></li>
+        </ul>
+    </div>
+    <div style="width: auto; float: left;">
+        <h1>Content</h1>
+    </div>
+</body>
+</html>
